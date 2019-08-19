3a6fe454ba61a074ac1fd434fc14c0ba478ea164
janakhpon <jnovaxer@gmail.com>
2019-08-19 18:07:48 +0630

    GDEV003

--- task/models.py
+++ task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Task(models.Model):
+    topic = models.CharField(max_length=50)
+    description = models.TextField()
+    date_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.topic
